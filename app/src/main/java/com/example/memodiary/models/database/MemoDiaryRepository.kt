567c1fc839610cbda598549ec111fa4e4d6b847b
sudhanshu <r02.sudhanshu@gmail.com>
2022-09-04 08:14:20 +0530

    created Dao and repository

---
+++ app/src/main/java/com/example/memodiary/models/database/MemoDiaryRepository.kt
@@ -0,0 +1,34 @@
+package com.example.memodiary.models.database
+
+import androidx.annotation.WorkerThread
+import com.example.memodiary.models.entities.MemoDiary
+
+/**
+ * A Repository manages queries and allows you to use multiple backends. In the most common example, the Repository implements the logic for deciding whether to fetch data from a network or use results cached in a local database.
+ */
+
+// Declares the DAO as a private property in the constructor. Pass in the DAO
+// instead of the whole database, because you only need access to the DAO
+class MemoDiaryRepository(private val memoDiaryDao: MemoDiaryDao) {
+    /**
+     * Denotes that the annotated method should only be called on a worker thread. If the annotated element is a class, then all methods in the class should be called on a worker thread.
+     */
+
+    // Room executes all queries on a separate thread.
+    // Observed Flow will notify the observer when the data has changed.
+//    val allWords: Flow<List<Word>> = wordDao.getAlphabetizedWords()
+
+
+    @WorkerThread
+    suspend fun insertMemoDiaryData(memoDiary: MemoDiary){
+        memoDiaryDao.insertFavMemoDiaryDetails(memoDiary)
+    }
+}
+
+/**
+ * A Flow is an async sequence of values
+
+Flow produces values one at a time (instead of all at once) that can generate values from async operations like network requests, database calls, or other async code. It supports coroutines throughout its API, so you can transform a flow using coroutines as well!
+
+By default, to avoid poor UI performance, Room doesn't allow you to issue queries on the main thread. When Room queries return Flow, the queries are automatically run asynchronously on a background thread.
+ */
---
+++ app/src/main/java/com/example/memodiary/models/database/MemoDiaryRoomDatabase.kt
@@ -0,0 +1,46 @@
+package com.example.memodiary.models.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.memodiary.models.entities.MemoDiary
+
+//@Database annotation is used to make it a room db.
+//entities = [MemoDiary::class] is an array of entities that we are gonna use inside our db.
+//version is whenever you make a change to your db structure, you need to migrate that and change the version up.
+//You can wipe all the data and then use the same version as you go but then all the data will be gone and you need to handle it via migration.This is why, it is not a recommended way of doing that.
+@Database(entities = [MemoDiary::class], version = 1)
+abstract class MemoDiaryRoomDatabase :RoomDatabase(){
+    companion object{
+//        Singleton prevents multiple instances of database opening at the same time.
+        @Volatile
+        private var INSTANCE : MemoDiaryRoomDatabase? = null
+
+        fun getDatabase(context : Context) : MemoDiaryRoomDatabase {
+            //if INSTANCE is not null, then return it
+            //if it is, then create the database
+            return INSTANCE ?: synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MemoDiaryRoomDatabase::class.java,
+                    "memo_diary_database"
+                ).build()
+                INSTANCE = instance
+                //return instance
+                instance
+            }
+        }
+
+    }
+
+    abstract fun memoDiaryDai() : MemoDiaryDao
+
+//    For each DAO class that is associated with the database, the database class must define an abstract method that has zero arguments and returns an instance of the DAO class.
+}
+
+/**
+ * Note: If your app runs in a single process, you should follow the singleton design pattern when instantiating an AppDatabase object. Each RoomDatabase instance is fairly expensive, and you rarely need access to multiple instances within a single process.
+
+ * If your app runs in multiple processes, include enableMultiInstanceInvalidation() in your database builder invocation. That way, when you have an instance of AppDatabase in each process, you can invalidate the shared database file in one process, and this invalidation automatically propagates to the instances of AppDatabase within other processes.
+**/
